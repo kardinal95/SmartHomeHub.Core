d501daf99e4d0a09b32eeebecb7e11ae8719e16b
Ivan <ustalovi@gmail.com>
2020-01-15 12:56:42 +0500

    Hotfix #2

--- py/srv/api/client/operations/devices.py
+++ py/srv/api/client/operations/devices.py
@@ -3,12 +3,13 @@ from py.srv.api.client.operations.rooms import get_room
 from py.srv.api.exceptions import *
 from py.srv.database import db_session
 from py.srv.database.models.device import DeviceMdl
+from py.srv.database.models.room import RoomMdl
 from py.srv.redis import RedisSrv
 
 
 @db_session
 def get_room_devices(uuid, acl, session):
-    room = get_room(uuid=uuid, session=session)
+    room = RoomMdl.get_room_with_uuid(uuid=uuid, session=session)
 
     devices = [x for x in room.devices if x.interface is not None and x.interface.read_acl <= acl]
     return devices
