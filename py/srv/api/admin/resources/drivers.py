4a0fb6488686fa2324b1c7728484a4dcdf9df2dd
Ivan <ustalovi@gmail.com>
2020-03-12 15:14:33 +0500

    Bugfix

--- py/srv/api/admin/resources/drivers.py
+++ py/srv/api/admin/resources/drivers.py
@@ -1,5 +1,6 @@
 from flask_restful import Resource
 
+from py.srv.api.admin.models.drivers import DriverDTO
 from py.srv.api.admin.operations.drivers import get_drivers
 from py.srv.api.exceptions import abort_on_exc
 from py.srv.database import db_session
@@ -10,4 +11,4 @@ class Drivers(Resource):
     @db_session
     def get(self, session):
         drivers = get_drivers(session=session)
-        return [x.as_json() for x in drivers]
+        return [DriverDTO(x).as_json() for x in drivers]
