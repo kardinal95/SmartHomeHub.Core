45e2b31fdbea16db306feed0f0366e35d7afc845
Ivan <ustalovi@gmail.com>
2020-04-10 15:46:00 +0500

    Bugfix

--- py/srv/api/admin/operations/endpoints.py
+++ py/srv/api/admin/operations/endpoints.py
@@ -37,6 +37,8 @@ def process_add(mods, session):
     for item in mods:
         if item['name'] == "":
             raise ApiOperationError("add", "Name cannot be empty", item)
+        if item['driver']['uuid'] == "":
+            raise ApiOperationError("add", "Driver cannot be empty", item)
         if DriverInstanceMdl.get_instance_by_uuid(uuid=item['driver']['uuid'], session=session) is None:
             raise ApiOperationError("add", "Cannot find specified driver", item['driver'])
         ep = EndpointMdl(uuid=uuid.UUID(item['uuid']),
