6bfdecd49c1d354124645a6ab37377543205d5b8
Ivan <ustalovi@gmail.com>
2020-04-22 14:19:45 +0500

    Finishing devices change api

--- py/srv/api/admin/operations/devices.py
+++ py/srv/api/admin/operations/devices.py
@@ -61,11 +61,68 @@ def process_add(mods, session):
 
 
 @db_session
+def process_delete(mods, session):
+    for item in mods:
+        dev = DeviceMdl.get_device_with_uuid(uuid=uuid.UUID(item),
+                                             session=session)
+        if dev is None:
+            raise ApiOperationError("Cannot find specified device", item)
+        try:
+            session.delete(dev)
+            session.flush()
+        except IntegrityError:
+            raise ApiOperationError(f"Cannot delete item: currently in use", item)
+
+
+@db_session
+def process_edit(mods, session):
+    for item in mods:
+        dev = DeviceMdl.get_device_with_uuid(uuid=uuid.UUID(item['uuid']),
+                                             session=session)
+        if item['name'] == "":
+            raise ApiOperationError("Name cannot be empty", item['uuid'])
+        dev.name = item['name']
+        if item['exported']:
+            if item['interface'] is None:
+                raise ApiOperationError("Interface parameters not specified", item['uuid'])
+            dev.interface = InterfaceMdl()
+            dev.interface.read_acl = item['interface']['read_acl']
+            dev.interface.write_acl = item['interface']['write_acl']
+        else:
+            dev.interface = None
+        if item['type'] == "":
+            raise ApiOperationError("Type cannot be empty", item['uuid'])
+        try:
+            ent = DeviceEntEnum[item['type']]
+            if ent != dev.dev_type:
+                dev.dev_type = DeviceEntEnum[item['type']]
+        except KeyError:
+            raise ApiOperationError("Incorrect device type", item['uuid'])
+        dev.sources = list()
+        for param in item['sources'].keys():
+            if item['sources'][param]['ep_param'] == "":
+                raise ApiOperationError('Endpoint parameter not specified', item['uuid'])
+            if item['sources'][param]['ep_uuid'] == "":
+                raise ApiOperationError('Endpoint uuid not specified', item['uuid'])
+            if EndpointMdl.get_endpoint_by_uuid(uuid=uuid.UUID(item['sources'][param]['ep_uuid']),
+                                                session=session) is None:
+                raise ApiOperationError('Endpoint with selected uuid not found', item['uuid'])
+            link = DeviceSourceMdl()
+            link.device_param = param
+            link.device_uuid = dev.uuid
+            link.endpoint_param = item['sources'][param]['ep_param']
+            link.endpoint_uuid = uuid.UUID(item['sources'][param]['ep_uuid'])
+            dev.sources.append(link)
+        session.add(dev)
+    session.flush()
+
+
+@db_session
 def process_modifications(mods, session):
     try:
         process_add(mods=mods['added'], session=session)
-        #devs['removed'] = (process_delete(mods=mods['removed'], session=session))
-        #devs['changed'] = (process_edit(mods=mods['changed'], session=session))
+        process_delete(mods=mods['removed'], session=session)
+        process_edit(mods=mods['changed'], session=session)
     except ApiOperationError as e:
         session.rollback()
         raise e
