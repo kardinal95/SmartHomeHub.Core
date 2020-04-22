7bffe25b1e5bca9a9376f6996fa77406f8148cf6
Ivan <ustalovi@gmail.com>
2020-04-22 14:08:39 +0500

    bugfix

--- py/srv/api/admin/resources/devices.py
+++ py/srv/api/admin/resources/devices.py
@@ -4,8 +4,8 @@ from flask_restful import Resource, reqparse, abort
 
 from py.srv.api.admin.models.devices import DeviceDTO
 from py.srv.api.admin.models.endpoints import EndpointDTO, get_required_params
-from py.srv.api.admin.operations.devices import get_all_devices
-from py.srv.api.admin.operations.endpoints import get_all_endpoints, get_endpoint_parameters, process_modifications
+from py.srv.api.admin.operations.devices import get_all_devices, process_modifications
+from py.srv.api.admin.operations.endpoints import get_all_endpoints, get_endpoint_parameters
 from py.srv.api.exceptions import abort_on_exc, ApiOperationError
 from py.srv.database import db_session
 
