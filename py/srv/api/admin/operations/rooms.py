81ac7ede3509df6cac76aa705feee7c34abe400a
Ivan <ustalovi@gmail.com>
2020-05-16 15:48:37 +0500

    Room device binding from API

--- py/srv/api/admin/operations/rooms.py
+++ py/srv/api/admin/operations/rooms.py
@@ -1,5 +1,9 @@
+from py.srv.api.exceptions import ApiOperationError
 from py.srv.database import db_session
+from py.srv.database.models.device import DeviceMdl
 from py.srv.database.models.room import RoomMdl
+
+from uuid import UUID
 
 
 @db_session
@@ -9,6 +13,34 @@ def get_all_rooms(session):
 
 @db_session
 def get_room_devices(uuid, session):
-    room = RoomMdl.get_room_with_uuid(uuid=uuid, session=session)
+    room = RoomMdl.get_room_with_uuid(uuid=UUID(uuid), session=session)
     devices = room.get_devices(session=session)
     return devices
+
+
+@db_session
+def process_add(uuid, session, mods):
+    room = RoomMdl.get_room_with_uuid(uuid=UUID(uuid), session=session)
+    for item in mods:
+        dev = DeviceMdl.get_device_with_uuid(uuid=UUID(item), session=session)
+        room.devices.append(dev)
+        session.add(room)
+    session.flush()
+
+
+@db_session
+def process_delete(uuid, session, mods):
+    room = RoomMdl.get_room_with_uuid(uuid=UUID(uuid), session=session)
+    room.devices = [x for x in room.devices if str(x.uuid) not in mods]
+    session.flush()
+
+
+@db_session
+def process_modifications(uuid, session, mods):
+    try:
+        process_add(uuid=uuid, mods=mods['added'], session=session)
+        process_delete(uuid=uuid, mods=mods['removed'], session=session)
+    except ApiOperationError as e:
+        session.rollback()
+        raise e
+    session.commit()
--- py/srv/api/admin/resources/rooms.py
+++ py/srv/api/admin/resources/rooms.py
@@ -1,8 +1,18 @@
-from flask_restful import Resource
+import uuid
 
-from py.srv.api.admin.operations.rooms import get_all_rooms, get_room_devices
-from py.srv.api.exceptions import abort_on_exc
+from flask_restful import Resource, reqparse, abort
+
+from py.srv.api.admin.operations.rooms import get_all_rooms, get_room_devices, process_modifications
+from py.srv.api.exceptions import abort_on_exc, ApiOperationError
 from py.srv.database import db_session
+
+
+parser = reqparse.RequestParser()
+parser.add_argument('mods',
+                    help='This field cannot be blank',
+                    type=dict,
+                    required=True,
+                    location='json')
 
 
 class AdmRooms(Resource):
@@ -20,5 +30,15 @@ class AdmRoomDevices(Resource):
     @abort_on_exc
     @db_session
     def get(self, room_uuid, session):
-        devices = get_room_devices(uuid=room_uuid, session=session)
+        devices = get_room_devices(uuid=uuid.UUID(room_uuid), session=session)
         return [str(x.uuid) for x in devices]
+
+    @abort_on_exc
+    @db_session
+    def post(self, room_uuid, session):
+        args = parser.parse_args()
+        try:
+            process_modifications(uuid=room_uuid, mods=args['mods'], session=session)
+        except ApiOperationError as e:
+            abort(400, message=e.as_json())
+        return
